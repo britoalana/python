b4e09e053c625194ea279dfbf03da6f3462551ae
britoalana <110117693+britoalana@users.noreply.github.com>
2025-03-05 18:02:12 -0300

    Update desafio4.py

--- desafio4.py
+++ desafio4.py
@@ -1,10 +1,13 @@
 notas = [7.5, 6.0, 9.0, 8.5, 10.0]
 nomes = ["João", "Maria", "Pedro", "Ana", "Lucas"]
 
-print("Notas dos alunos:")
+print("\nNotas dos alunos:\n")
+for nome, nota in zip (nomes, notas):
+    print(f'{nome}: {nota}')
 
 media = sum(notas) / len(notas) 
-print(f"Média das notas: {media :.2f}")
+print(f"\nMédia das notas: {media :.2f}")
 
-maiornota = max(notas)
-print(f"Tirou a melhor nota {maiornota}")
+maior_nota = max(notas)
+melhor = [nomes[i] for i, nota in enumerate(notas) if nota == maior_nota]
+print(f"\n{' '.join(melhor)} tirou a melhor nota {maior_nota}\n")
